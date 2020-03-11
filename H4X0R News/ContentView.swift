9081f6aa92e90b0345dbecec832c4d8ea010d792
agil <antonio_g_s@hotmail.com>
2020-03-11 22:32:53 +0100

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Antonio Gil Sánchez on 11/03/2020.
+//  Copyright © 2020 Antonio Gil Sánchez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

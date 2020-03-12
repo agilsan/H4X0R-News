d67ba31f0d2e85ba53bcd05f41f59c1928adc12d
agil <antonio_g_s@hotmail.com>
2020-03-12 23:45:10 +0100

    Final

---
+++ H4X0R News/Views/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  H4X0R News
+//
+//  Created by Antonio Gil Sánchez on 11/03/2020.
+//  Copyright © 2020 Antonio Gil Sánchez. All rights reserved.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView : UIViewRepresentable {
+    let urlString : String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
